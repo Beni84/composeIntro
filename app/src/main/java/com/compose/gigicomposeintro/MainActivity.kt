fae5ecb0b2e6a02d1329add5dc20539716147dae
Jose Benito <jose.benito@gigigo.com>
2021-05-17 12:57:53 +0200

    Updates exercises

--- app/src/main/java/com/compose/gigicomposeintro/MainActivity.kt
+++ app/src/main/java/com/compose/gigicomposeintro/MainActivity.kt
@@ -11,18 +11,20 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.compose.gigicomposeintro.ui.theme.GigiComposeIntroTheme
 
 /* 1 - Crear una función que contenga un texto
-   2 - Pasar una parámetro a la función
-   3 - Que se vea una vista previa
+   2 - Que se vea una vista previa
+   3 - Pasar una parámetro a la función
    3a - Salude a varios usuarios
-   4 - Añadir 2 textos más
    5 - Visualizar como listado
-   6 - Añadir padding al listado
+   6 - Añadir padding y background al listado
    7 - Añadir una imagen como primer elemento MaxWidth y 180dp altura
    8 - Añadir espacio entre elementos
+   8a - Añadir background a cada texto
+   8b - Añadir espacio entre color y texto
    9 - Redondear bordes imagen
    10 - Estilo texto
    11 - Máximo 2 líneas
-   12 - Mostrar boton 
+   12 - Mostrar boton con Leer Más y alinear a la derecha
+   13 - Se muestra una Toast cuando se muestra un boton.
  */
 
 class MainActivity : ComponentActivity() {
