ca81885eb0782390eaccd47f4405fcabc2c85b93
Jose Benito <jose.benito@gigigo.com>
2021-05-17 12:59:06 +0200

    Solved

--- app/src/main/java/com/compose/gigicomposeintro/MainActivity.kt
+++ app/src/main/java/com/compose/gigicomposeintro/MainActivity.kt
@@ -1,14 +1,36 @@
 package com.compose.gigicomposeintro
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.Snackbar
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment.Companion.End
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.ContentScale.Companion
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow.Clip
+import androidx.compose.ui.text.style.TextOverflow.Ellipsis
 import androidx.compose.ui.tooling.preview.Preview
-import com.compose.gigicomposeintro.ui.theme.GigiComposeIntroTheme
+import androidx.compose.ui.unit.dp
 
 /* 1 - Crear una función que contenga un texto
    2 - Que se vea una vista previa
@@ -31,7 +53,67 @@ class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-
+      MultipleGreeting(emptyList())
     }
   }
+}
+
+@Composable
+fun Greeting(name: String) {
+  Text(
+    text = "Hello, ${name}!",
+    color = Color.White,
+    modifier = Modifier
+      .padding(vertical = 4.dp)
+      .background(Color.DarkGray)
+      .padding(4.dp),
+    style = MaterialTheme.typography.body1
+  )
+}
+
+@Composable
+fun MultipleGreeting(names: List<String>) {
+  val context = LocalContext.current
+
+  Column(
+    modifier = Modifier
+      .background(Color.White)
+      .padding(16.dp)
+  ) {
+    Image(
+      painter = painterResource(id = R.drawable.header),
+      contentDescription = null,
+      modifier = Modifier
+        .height(180.dp)
+        .fillMaxWidth()
+        .clip(RoundedCornerShape(32.dp)),
+      contentScale = ContentScale.Crop
+    )
+    Spacer(modifier = Modifier.height(16.dp))
+    Text(
+      text = "Nombres que han ganado un premio en la tombola del barrio de al lado de mi casa:",
+      style = MaterialTheme.typography.h6,
+      maxLines = 2,
+      overflow = Ellipsis
+    )
+    Spacer(modifier = Modifier.height(16.dp))
+    names.forEach {
+      Greeting(it)
+    }
+    Button(
+      onClick = {
+        Toast.makeText(context, "Button is clicked", Toast.LENGTH_LONG).show()
+      },
+      modifier = Modifier.align(End)
+    ) {
+      Text(text = "Leer Más")
+    }
+  }
+}
+
+@Preview
+@Composable
+fun Preview() {
+  val names = listOf("Pepe", "Jose", "Pepillo")
+  MultipleGreeting(names)
 }
